41fbcfce51483918dd38a08dca5119564a93d049
Kelsey RIDER <krider@ineat.fr>
2019-11-20 16:19:22 +0000

    feat(calc): A harder implementation of add

--- src/main/java/com/ineat/academy/unittests/util/Calculator.java
+++ src/main/java/com/ineat/academy/unittests/util/Calculator.java
@@ -2,10 +2,24 @@ package com.ineat.academy.unittests.util;
 
 import org.springframework.stereotype.Component;
 
+import java.math.BigInteger;
+
 @Component
 public class Calculator {
     public int add(Integer x, Integer y) {
-        return x + y;
+        BigInteger bx = BigInteger.valueOf(x);
+        BigInteger by = BigInteger.valueOf(y);
+        BigInteger result = BigInteger.ZERO;
+
+        for(int i=0; i<=Math.max(bx.bitCount(), by.bitCount()); i++) {
+            if(bx.testBit(i) ^ by.testBit(i)) {
+                result = result.setBit(i);
+            } else {
+                result = result.clearBit(i);
+            }
+        }
+
+        return result.intValue();
     }
 
     public int subtract(Integer x, Integer y) {
